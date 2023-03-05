ec022562619f48e739bb4a2975870bd1b1caa8b3
Srishti Kamra <srishtikamra07@gmail.com>
2023-03-04 21:21:28 -0600

    Initial Commit

---
+++ API Calling 2/API_Calling_2App.swift
@@ -0,0 +1,17 @@
+//
+//  API_Calling_2App.swift
+//  API Calling 2
+//
+//  Created by Srishti Kamra  on 3/4/23.
+//
+
+import SwiftUI
+
+@main
+struct API_Calling_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ API Calling 2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  API Calling 2
+//
+//  Created by Srishti Kamra  on 3/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
